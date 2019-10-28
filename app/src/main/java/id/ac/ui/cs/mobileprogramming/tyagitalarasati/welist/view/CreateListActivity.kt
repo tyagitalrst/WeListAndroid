e58409d72093c68434b4c7c9f2f477cee77d2f69
tyagita <tiagita@gmail.com>
2019-10-29 00:35:09 +0700

    Initialized commit

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/tyagitalarasati/welist/view/CreateListActivity.kt
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.R
+
+class CreateListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_list)
+    }
+}
