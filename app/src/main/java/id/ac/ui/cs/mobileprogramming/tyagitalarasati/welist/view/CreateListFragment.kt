ce9eaa7500d19a547af202c9e0aa3914e67f4b66
tyagita <tiagita@gmail.com>
2019-10-30 03:26:47 +0700

    Implementation of create new list

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/tyagitalarasati/welist/view/CreateListFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/tyagitalarasati/welist/view/CreateListFragment.kt
@@ -1,64 +1,70 @@
 package id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.view
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
 
 import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.R
 import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.model.WeList
 import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.viewmodel.WeListViewModel
-import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_create_list.*
+
+
 
 /**
  * A simple [Fragment] subclass.
  */
-class ListFragment : Fragment() {
+class CreateListFragment : Fragment() {
 
     companion object {
-        fun newInstance() = ListFragment()
+        fun newInstance() = CreateListFragment()
     }
 
     private lateinit var viewModel: WeListViewModel
-    private val weListAdapter = WeListAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(R.layout.fragment_create_list, container, false)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(WeListViewModel::class.java)
 
-        weLists.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = weListAdapter
-        }
 
-        observeViewModel()
-
-        floatingActionButton.setOnClickListener{
+        buttonSubmit.setOnClickListener{
+            saveNote()
             Navigation.findNavController(it)
-                .navigate(ListFragmentDirections.actionCreateList())
+                .navigate(CreateListFragmentDirections.actionResultCreate())
         }
 
     }
 
 
-    fun observeViewModel() {
-        viewModel.getAllWeList().observe(this,
-            Observer<List<WeList>> { t -> weListAdapter.setWeLists(t!!) })
+    fun saveNote() {
+        if (editTextTitle.text.toString().trim().isBlank()
+            || editTextNotes.text.toString().trim().isBlank()) {
+            Toast.makeText(getActivity(),"Can not insert empty note!", Toast.LENGTH_SHORT).show()
+        }
+
+        val newWeList = WeList(editTextTitle.text.toString(),
+                            editTextNotes.text.toString(),
+                            editTextPrice.text.toString(),
+                            editTextLink.text.toString())
+
+        viewModel.insert(newWeList)
+        Toast.makeText(getActivity(),"New list", Toast.LENGTH_SHORT).show()
+
     }
 
 
