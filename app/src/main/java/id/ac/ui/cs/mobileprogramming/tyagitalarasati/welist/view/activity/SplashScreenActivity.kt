e4450eacbdecd376927bb4723eeee8fb55a73d32
tyagita <tiagita@gmail.com>
2019-12-26 02:05:00 +0700

    Final revision of notification

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/tyagitalarasati/welist/view/activity/SplashScreenActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/tyagitalarasati/welist/view/activity/SplashScreenActivity.kt
@@ -11,6 +11,7 @@ import android.os.Handler
 import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.R
 import id.ac.ui.cs.mobileprogramming.tyagitalarasati.welist.util.NotificationPublisher
 
@@ -18,13 +19,16 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private val NOTIFICATION_CHANNEL_ID = "10001"
     private val default_notification_channel_id = "default"
-    private val SPLASH_TIME_OUT:Long = 3000
+    private val SPLASH_TIME_OUT:Long = 5000
+    private val id = (0..1).random()
+    private val listOfNotif = listOf<String>("Let's check your long term wishlist & make it come true!",
+        "Let's check your short term wishlist & make it come true!")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
 
-        scheduleNotification(getNotification( "Welcome to WeList. Let's create your wishlist!" ) , 5000 )
+        scheduleNotification(getNotification( listOfNotif.get(id) ) , 10000 )
 
         Handler().postDelayed({
             startActivity(Intent(this,
@@ -50,12 +54,20 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun getNotification(content: String): Notification {
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = TaskStackBuilder.create(application)
-            .addNextIntent(intent)
-            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        var fragment = 0
+        if (id.equals(0)) {
+            fragment = R.id.listLongTermFragment
+        } else {
+            fragment = R.id.listFragment
+        }
+        val pendingIntent = NavDeepLinkBuilder(applicationContext)
+            .setComponentName(MainActivity::class.java)
+            .setGraph(R.navigation.navigation)
+            .setArguments(Bundle())
+            .setDestination(fragment)
+            .createPendingIntent()
         val builder = NotificationCompat.Builder(this, default_notification_channel_id)
-        builder.setContentTitle("WeList Notification")
+        builder.setContentTitle("Hey, Don't Forget!")
         builder.setContentText(content)
         builder.setSmallIcon(R.mipmap.ic_launcher_round)
         builder.setAutoCancel(true)
